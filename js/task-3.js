930e4be7402b5dd23278b5caf07e1f51b1925c1a
Andrew <36772492+klimkovskyi@users.noreply.github.com>
2024-03-25 17:29:57 +0200

    hw4 js task

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,15 @@
-function checkForSpam(message) {
-    return (message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale')); 
+function filterArray(numbers, value) {
+    let newArray = [];
+    for (const number of numbers) {
+        if (number > value) {
+            newArray.push(number); 
+        }
+    }
+    return newArray;
 }
 
-console.log(checkForSpam("Latest technology news"));
-console.log(checkForSpam("JavaScript weekly newsletter"));
-console.log(checkForSpam("Get best sale offers now!"));
-console.log(checkForSpam("Amazing SalE, only tonight!"));
-console.log(checkForSpam("Trust me, this is not a spam message"));
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
-console.log(checkForSpam("[SPAM] How to earn fast money?"));
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
